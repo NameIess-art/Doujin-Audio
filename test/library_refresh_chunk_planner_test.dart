286291aa5f1e5c7dda4cf920e1cfc82add457e4d
nameless <schikejaaron@gmail.com>
2026-06-13 01:19:23 +0800

    refactor: extract library refresh chunk planning

---
+++ test/library_refresh_chunk_planner_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nameless_audio/models/music_track.dart';
+import 'package:nameless_audio/services/library_refresh_chunk_planner.dart';
+
+void main() {
+  MusicTrack track(int index) => MusicTrack(
+    path: '/music/$index.mp3',
+    displayName: '$index',
+    groupKey: '/music',
+    groupTitle: 'music',
+    groupSubtitle: '/music',
+    isSingle: false,
+  );
+
+  test('refresh chunks defer structural changes and totals to final chunk', () {
+    final chunks = buildLibraryRefreshChunks(
+      sourceFolderPath: '/music',
+      libraryRoot: '/music',
+      sourceLabel: 'music',
+      tracks: List<MusicTrack>.generate(3, track),
+      folderPaths: const <String>['/music/album'],
+      removeWatchedFolders: const <String>['/music/old'],
+      addWatchedFolders: const <String>['/music/album'],
+      removeTrackPaths: const <String>['/music/deleted.mp3'],
+      removeEntryPaths: const <String>['/music/deleted'],
+      duplicateCount: 2,
+      failureCount: 1,
+      progressPrefix: '[1/1]',
+      chunkSize: 2,
+    );
+
+    expect(chunks, hasLength(2));
+    expect(chunks.first.tracks, hasLength(2));
+    expect(chunks.first.removeTrackPaths, isEmpty);
+    expect(chunks.first.duplicateCount, 0);
+    expect(chunks.last.tracks, hasLength(1));
+    expect(chunks.last.removeTrackPaths, const <String>['/music/deleted.mp3']);
+    expect(chunks.last.removeEntryPaths, const <String>['/music/deleted']);
+    expect(chunks.last.duplicateCount, 2);
+    expect(chunks.last.failureCount, 1);
+    expect(chunks.last.progressLabel, '[1/1] [3/3] music');
+  });
+
+  test('empty refresh still emits one structural update chunk', () {
+    final chunks = buildLibraryRefreshChunks(
+      sourceFolderPath: '/music',
+      libraryRoot: '/music',
+      sourceLabel: 'music',
+      tracks: const <MusicTrack>[],
+      folderPaths: const <String>[],
+      removeWatchedFolders: const <String>[],
+      addWatchedFolders: const <String>[],
+      removeTrackPaths: const <String>['/music/deleted.mp3'],
+      removeEntryPaths: const <String>[],
+      duplicateCount: 0,
+      failureCount: 0,
+      progressPrefix: '',
+    );
+
+    expect(chunks, hasLength(1));
+    expect(chunks.single.tracks, isEmpty);
+    expect(chunks.single.removeTrackPaths, const <String>[
+      '/music/deleted.mp3',
+    ]);
+    expect(chunks.single.progressLabel, 'music');
+  });
+}

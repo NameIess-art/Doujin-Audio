26c947da5d489e6cefaaf52e77d3fd39758846b7
nameless <schikejaaron@gmail.com>
2026-07-21 23:00:25 +0800

    Fix Flutter Android integration launch discovery

--- test/android_manifest_regression_test.dart
+++ test/android_manifest_regression_test.dart
@@ -14,4 +14,19 @@ void main() {
       isNot(contains('android:windowSoftInputMode="adjustResize"')),
     );
   });
+
+  test('Flutter tooling can launch the app without duplicating its icon', () {
+    final manifest = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync();
+
+    expect(manifest, contains('android:name=".MainActivity"'));
+    expect(manifest, contains('android.intent.action.MAIN'));
+    expect(manifest, contains('android.intent.category.LAUNCHER'));
+    expect(
+      manifest,
+      contains('android:scheme="\${applicationId}.integration-test"'),
+    );
+    expect(manifest, contains('<activity-alias'));
+  });
 }
